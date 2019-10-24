dc703ec7b07be917931cda26505e72b557193be5
rudalson <rudalson@gmail.com>
2019-10-24 14:11:17 +0900

    updated hobby

--- matching/hobby.py
+++ matching/hobby.py
@@ -3,15 +3,38 @@ import sys
 list_hobby = []
 
 
+class Solution:
+    def __init__(self):
+        self.max_matched = 0
+        self.couple_set = set()
+
+    def get_max_matched(self):
+        return self.max_matched
+
+    def add(self, first, second, count):
+        if count < self.max_matched:
+            return
+        elif count > self.max_matched:
+            self.max_matched = count
+            self.couple_set.clear()
+
+        self.couple_set.add((first, second))
+
+
 # dic = {}
+step = 2
+previous_value = 0
+hypo_matched = 0
+solution = Solution()
 
 
-def method1(hobbys):
+def find_best_couple(hobbys):
     matched_count = 0
     for i in range(len(hobbys) - 1):
         basis = hobbys[i]
         matched_count = basis & hobbys[i + 1]
-        print("{} : ({}, {}) - {}. count({})".format(i, basis, hobbys[i + 1], matched_count, count_set_bits(matched_count)))
+        print("{} : ({}, {}) - {}. count({})".format(i, basis, hobbys[i + 1], matched_count,
+                                                     count_set_bits(matched_count)))
 
     # print("list2 :", list_member)
 
@@ -24,12 +47,17 @@ def count_set_bits(n):
     return count
 
 
+
 def convert_numeric(characters):
     value = 0
     for c in characters:
         value |= 1 << ord(c) - ord('A')
 
     list_hobby.append(value)
+
+    count = count_set_bits(previous_value & value)
+    previous_value = value
+    t = (hypo_matched if hypo_matched > count else count)
 
     # if dic.get(value) is None:
     #     dic[value] = [len(list_hobby)]
@@ -47,12 +75,9 @@ def load_data(filename):
     for line in lines[1:]:
         characters = line.rstrip().split(" ")
         convert_numeric(characters)
-    # print(list_hobby)
+    print(list_hobby)
 
-    sorted_hobby = list_hobby.copy()
-    sorted_hobby.sort()
-    print(sorted_hobby)
-    method1(sorted_hobby)
+    find_best_couple(list_hobby)
 
 
 if __name__ == '__main__':
